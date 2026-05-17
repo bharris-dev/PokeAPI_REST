1166aeadcc264ac568efd855ceaedb680bd33272
bharris-dev <brandonlharris2002@gmail.com>
2026-05-17 02:00:42 +0100

    Added Pokemon class, assign class variable values within assign_pokemon()

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import requests
+from dataclasses import dataclass
 
 base_url = "https://pokeapi.co/api/v2"
+
+@dataclass
+class Pokemon:
+    id: int = 0
+    name: str = ""
+    types: list[str] = None
+    hp: int = 0
+    attack: int = 0
+    special_attack = 0
+    defence: int = 0
+    special_defence = 0
+    moves = {}
 
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
@@ -13,14 +26,51 @@ def get_pokemon_info(name):
         print(f"Error code: {response.status_code}")
         return None
 
+def assign_pokemon(poke_info):
+    current_pokemon: Pokemon = Pokemon()
+
+    current_pokemon.id = pokemon_info['id']
+    current_pokemon.name = pokemon_info['name']
+    current_pokemon.types = pokemon_info['types']
+
+    types: list = []
+
+    for t in pokemon_info["types"]:
+        type_name = t["type"]["name"]
+        types.append(type_name)
+
+    current_pokemon.types = types
+
+    stats = {}
+
+    # Dict Key
+    for stat in pokemon_info["stats"]:
+        stat_name = stat["stat"]["name"]
+        stat_value = stat["base_stat"]
+
+        stats[stat_name] = stat_value
+
+    # Dict value
+    current_pokemon.hp = stats["hp"]
+    current_pokemon.attack = stats["attack"]
+    current_pokemon.special_attack = stats["special-attack"]
+    current_pokemon.defence = stats["defense"]
+    current_pokemon.special_defence = stats["special-defense"]
+
+    moves = {}
+    for move in pokemon_info["moves"]:
+        move_name = move["name"]
+
+    print(current_pokemon)
 
 pokemon_name = input("Input pokemon name: ").lower()
 pokemon_info = get_pokemon_info(pokemon_name)
 
 if pokemon_info:
-    print(f"Name: {pokemon_info['name'].capitalize()}")
-    print(f"ID: {pokemon_info['id']}")
-    print(f"Height: {pokemon_info['height']}")
-    print(f"Weight: {pokemon_info['weight']}")
+    print(pokemon_info.keys())
+    print("Stats:", pokemon_info["stats"])
+    print("Moves", pokemon_info['moves'])
+
+    assign_pokemon(pokemon_info)
 else:
     print("Pokémon not found or API error.")
